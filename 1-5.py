7ca876687159b3afa4b43ae992d257ffebdf2c34
gaopeiru <ac1bca2@gmail.com>
2021-01-25 16:53:55 +0800

    今天練習到1-7

---
+++ 1-5.py
@@ -0,0 +1,7 @@
+#print(63 / 5)純除法
+#print(63 // 5)整數除法
+#print(63 % 5)得到除法的餘數
+n=int(input())
+k=int(input())
+print(k//n)
+print(k%n)
---
+++ 1-6.py
@@ -0,0 +1,6 @@
+# Read an integer:
+# a = int(input())
+# Print a value:
+# print(a)
+second=int(input())
+print((second//(60*60), second/60))
