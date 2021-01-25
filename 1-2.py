83ef429bf318d11affb64901af44e47f7615455a
gaopeiru <ac1bca2@gmail.com>
2021-01-25 15:45:45 +0800

    first commit

---
+++ 1-2.py
@@ -0,0 +1,5 @@
+# Read the numbers b and h like this:
+b = int(input())
+h = int(input())
+print(int(b*h*1/2))
+# Print the result with 
---
+++ test1.py
@@ -0,0 +1,8 @@
+print("Hello 1545")
+# 練習
+#print('hee')
+print("it's a dog")
+print('1234'+'250')
+print(100-80)
+print("100"+str(200))
+print(float("100")+200)
