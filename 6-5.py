3cae02710a24f5e9d55d67cdf1a99c0b9eafa9f0
gaopeiru <ac1bca2@gmail.com>
2021-01-27 11:23:33 +0800

    練習6-f

---
+++ 6-5.py
@@ -0,0 +1,16 @@
+a=int(input())
+i=0
+while a!=0:
+    i+=1
+    a=int(input())
+print(i)
+
+#累加程式
+#a=int(input())
+#i=0
+#sum=0
+#while a!=0:
+    #i+=1
+    #sum+=a
+    #a=int(input())
+#print(i,sum)
