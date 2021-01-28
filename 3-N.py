4f514588c51345c8a6b4a786d5ef51dbd97095d3
gaopeiru <ac1bca2@gmail.com>
2021-01-28 15:24:12 +0800

    更新

--- 3-N.py
+++ 3-N.py
@@ -4,8 +4,15 @@ x2=int(input())
 y2=int(input())
 x3=int(input())
 y3=int(input())
-x4=int()
-y4=int()
-if x1==x2 or x1==x3:
-    x1==x2
-    if 
+x4=0
+y4=0
+if x1==x2:
+    x4==x3
+elif x1==x3:
+    x4==x2
+if y1==y2:
+    y4==y3
+elif y1==y3:
+    y4==y2
+print(x4,y4)
+
