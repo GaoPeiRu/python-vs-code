3b319366231cf68373c0c56ae0646b87684b917d
gaopeiru <ac1bca2@gmail.com>
2021-01-28 09:15:40 +0800

    更改

--- 3-K.py
+++ 3-K.py
@@ -1,25 +1,9 @@
 month=int(input())
-if a<13:
-    if month==2:
-        year=int(input())
-        if year%4==0:
-            if year%100==0:
-                if year%400==0:
-                    print("29")
-                else:
-                    print("28")
-            else:
-                print("29")
-        else:
-            print("28")
-    elif month>=8:
-        if month%2==0:
-            print("31")
-        else:
-            print("30")
-    elif month%2==0:
-        print("30")
-    else:
-        print("31")
+day=int(input())
+month<13
+if month in [1,3,5,7,8,10,12] and day==31:
+    print(month+1,1)
+elif month in [4,6,9,11] and day==30:
+    print(month+1,1)
 else:
-    print(a)
+    print(month,day+1)
