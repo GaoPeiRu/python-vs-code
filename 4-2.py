18439887ee4210089b7171caaf0c830ccd043b78
gaopeiru <ac1bca2@gmail.com>
2021-01-29 11:24:43 +0800

    第四章更新

--- 4-2.py
+++ 4-2.py
@@ -1,9 +1,24 @@
-a=int(input())
-b=int(input())
-if a<b:
-    for i in range(a,b+1):
-        append(x)
-elif a>=b:
-    for i in range(b,a+1):
-        append(x)
-        reverse()
+a = int(input())
+b = int(input())
+lst=[]
+if a < b:
+  for x in range(a, b + 1):
+    lst.append(x)
+elif a >= b:
+  for x in range(b, a + 1):
+    lst.append(x)
+  lst.reverse() 
+
+for x in lst:
+  print (x)
+
+
+#a = int(input())
+#b = int(input())
+
+#if a < b:
+  #for i in range(a, b + 1):
+    #print(i, end=' ')
+#else:
+  #for i in range(a, b - 1, -1):
+    #print(i, end=' ')
