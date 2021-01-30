84deb8842b7c6e3a980a3ebc5c94f09568b8450f
gaopeiru <ac1bca2@gmail.com>
2021-01-30 14:20:28 +0800

    第六章更新

--- 6-G.py
+++ 6-G.py
@@ -1,17 +1,25 @@
-import math
-
 x = int(input())
-
-phi = 1.61803399
-sqrt5 = math.sqrt(5)
-
-def F(n):
-  return int((phi**n - (1-phi)**n) / sqrt5)
-    
-def isFibonacci(z):
-  return F(int(math.floor(math.log(sqrt5 * z,phi) + 0.5))) == z
-
-if isFibonacci(x) == True:
-  print(round(math.log(x * sqrt5 + 0.5) / math.log(phi)))
+t_n = 1
+t_prev =1
+fib=[1,1]
+n = 1
+while t_n < x:
+  t_n = fib[-1] + fib[-2]
+  fib.append(t_n)
+  n = n + 1
+if t_n == x:
+  print(fib.index(fib[-1])+1)
 else:
   print(-1)
+
+
+#prev, next = 1, 1
+#index = 2
+#possible_fib = int(input())
+#while possible_fib > next:
+  #prev, next = next, prev + next
+  #index += 1
+#if possible_fib == next:
+  #print(index)
+#else:
+  #print(-1)
