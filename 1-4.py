6b2cd14514737de05c1f7ed93d971f3cf01eeafc
GaoPeiRu <77956657+GaoPeiRu@users.noreply.github.com>
2025-06-10 11:01:53 +0800

    Update 1-4.py

--- 1-4.py
+++ 1-4.py
@@ -1,6 +1,6 @@
 a = int(input())
 print(" The next next number for the number",a,"is",(a+1))
-print(" The previous number for the number60",a,"is",(a-1))
+print(" The previous number for the number",a,"is",(a-1))
 
 #n = int(input())
 #print('The next number for the number ' + str(n) + ' is ' + str(n + 1))
